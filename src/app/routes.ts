0beda36e40b0f731952295985e7cecae74509662
jav-19jm <bonillajosemanuel2005@gmail.com>
2026-04-20 15:13:30 -0500

    definicion de la estructura del proyecto

---
+++ src/app/routes.ts
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+import { LandingPage } from "./pages/LandingPage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: LandingPage,
+  },
+
+]);
